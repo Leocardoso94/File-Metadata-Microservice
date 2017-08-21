e1029b7278d06ebf44c37b611970ecc30d972f94
Leonardo Cardoso <leonardo.cardoso@one.intranet>
2017-08-21 11:32:46 -0300

    ajustes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-
+const uploadFolder = './uploads/';
 const express = require('express')
 const app = express()
 const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: uploadFolder })
 const port = process.env.PORT || 8080
 const fs = require('fs')
 app.use(express.static('public'))
@@ -11,28 +11,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html')
 })
 
-const deleteFolderRecursive = function (path) {
-  if (fs.existsSync(path)) {
-    fs.readdirSync(path).forEach(function (file, index) {
-      var curPath = path + "/" + file
-      if (fs.lstatSync(curPath).isDirectory()) { // recurse
-        deleteFolderRecursive(curPath)
-      } else { // delete file
-        fs.unlinkSync(curPath)
-      }
-    })
-    fs.rmdirSync(path)
-  }
-}
-
 app.post('/upload', upload.single('file'), function (req, res) {
-  const size = req.file.size
-  deleteFolderRecursive('./uploads')
+  const file = req.file
+  const size = file.size
+  fs.unlink(uploadFolder + file.filename)
   res.json({
     size: size
   })
 })
-
 
 const listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port)
